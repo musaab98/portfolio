15dd204abb344bb348e661df376c1f2f81cc3d63
Musaab Elsheikh <musaab@Musaabs-MacBook-Air.local>
2023-09-23 21:16:51 -0400

    url strulling

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,34 @@
 export const projects = [
     {
       id: 1,
-      name: "Afghan Business",
-      description: `Business`,
-      link: "http://afghan-business-client-bucket.s3-website-us-east-1.amazonaws.com/",
-      tools: ["React", "JavaScript", "CSS", "HTML"],
-    },
-    {
-      id: 2,
       name: "VoltWise Motors",
       description: "Mock company website",
       link: "https://musaab98.github.io/voltwise-serverless-fs/",
       tools: ["React", "JavaScript", "CSS"],
     },
     {
+      id: 4,
+      name: "Khutbah Central",
+      description: "Khutbah.",
+      link: null,
+      tools: ["React", "JavaScript", "CSS"],
+    },
+    {
       id: 3,
+      name: "MCCL Soccer",
+      description: "Soccer.",
+      link: null,
+      tools: ["React", "JavaScript", "CSS"],
+    },
+    {
+      id: 2,
+      name: "Afghan Business",
+      description: `Business`,
+      link: "http://afghan-business-client-bucket.s3-website-us-east-1.amazonaws.com/",
+      tools: ["React", "JavaScript", "CSS", "HTML"],
+    },
+    {
+      id: 0,
       name: "Todo-List",
       description: "Todo list react app.",
       link: "https://musaab98.github.io/todo-list/",
